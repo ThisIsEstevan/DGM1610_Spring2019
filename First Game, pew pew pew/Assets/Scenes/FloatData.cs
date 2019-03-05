7a7d37d006c31da91576686823e08565fff9d603
estevan <eestevanrodriguez@gmail.com>
2019-03-05 13:46:07 -0700

    commit 1

---
+++ First Game, pew pew pew/Assets/Scenes/FloatData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+    public float Value;
+
+    public void OnEnable()
+    {
+        
+    }
+}
